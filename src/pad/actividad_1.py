7afa4cf90a74d4a42acabdceda9d7eeacc52a366
LinaMarcela14 <lina.roldan@est.iudigital.edu.co>
2025-02-26 18:19:20 +0000

    actividad1

--- src/pad/actividad_1.py
+++ src/pad/actividad_1.py
@@ -49,75 +49,35 @@ class Actividad1():
 
     def leer_api(self, url):
         # El get()método envía una solicitud de GET a la url especificada.
-        response = requests.get("https://exoplanetarchive.ipac.caltech.edu/TAP/sync")
+        response = requests.get("https://www.amiiboapi.com/api/amiibo/?name=zelda")
         return response.json()
 
-    
-    def  leer_api(self,url):
-        url = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync'
-        query = """SELECT * FROM planet WHERE pl_status='C'""" 
-        params = {
-        'request': 'doQuery',
-        'lang': 'ADQL',
-         'format': 'txt',
-        'query': query
-    }
-
-        if response.status_code == 200:
-            print ("Datos obtenidos:")
-            print(response.txt)
-
-        else:
-             print(f"Error {response.status_code}: No se pudo obtener los datos.")
-
-
-        response = requests.get(url)
-        return response.txt()
-    print("Datos en formato txt:")
-       
 
         
     def escribir_json(self,datos):
-        pass
+        with open("datos.json","w")as archivo:
+            json.dump(datos,archivo,incident=4)
+            #indent=4 para un formato legible
     
-    def escribir_txt(self,nombre,datos):
-        # r read w write
-
-        ruta_txt = "{}.txt".format(nombre)
-        datos=""
-        with open(ruta_txt,"w",encoding="utf-8") as f:
-            #f.write(datos)
-            f.writelines(datos)
-
+    def escribir_txt(self,nombre_archivo="",datos=None): # "" '' """ """
+        if nombre_archivo=="":
+            nombre_archivo="datos.txt"
+        if datos is None:
+            datos = "No hay datos"
+        ruta_txt = "{}/txt/{}".format(self.ruta_static,nombre_archivo)
+        with open(ruta_txt, 'w', encoding='utf-8') as f:
+            json.dump(datos, f, ensure_ascii=False, indent=4)
+            f.write(str(datos))
+        return True # booleano True (1) False (0)
 
 ingestion =Actividad1()
-#datos_json = Actividad1.leer_api("https://exoplanetarchive.ipac.caltech.edu/TAP/sync")
-#"https://exoplanetarchive.ipac.caltech.edu/TAP/sync"
-datos_json = ingestion.leer_api("https://exoplanetarchive.ipac.caltech.edu/TAP/sync")
+#datos_json = Actividad1.leer_api("https://www.amiiboapi.com/api/amiibo/?name=zelda")
+#"https://www.amiiboapi.com/api/amiibo/?name=zelda"
+datos_json = ingestion.leer_api("https://www.amiiboapi.com/api/amiibo/?name=zelda")
 print("datos json:",datos_json)
-if ingestion.escribir_json(nombre_archivo="entrega_actividad1.json",datos=datos_json):
-    print("se creo el archivo json")
+#if ingestion.escribir_json(nombre_archivo="entrega_actividad1.json",datos=datos_json):
+ #   print("se creo el archivo json")
+ingestion.escribir_txt("Actividad1.txt",datos_json) 
+ingestion.escribir_json("Actividad1.json",datos_json)
 
 
-
-
-inges = Actividad1
-datos_json = inges.leer_json()
-print(datos_json)
-print("************************************************************")
-print("************************************************************")
-datos_txt = inges.leer_txt()
-print(datos_txt)
-print("************************************************************")
-print("************************************************************")
-nombre_archivo = "info copy.txt"
-datos_txt_dos = inges.leer_varios_txt(nombre_archivo)
-print(datos_txt_dos)
-
-#inges.escribir_txt(nombre="archivo_json",datos=datos_json)
-#inges.escribir_txt(nombre="archivo_txt",datos=datos_txt)
-#inges.escribir_txt(nombre="archivo_txt_copy",datos=datos_txt_dos)
-    
-    
-    
-    
