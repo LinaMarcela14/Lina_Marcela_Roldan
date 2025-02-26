cd6dce7705ba465970d44f87f6758142a52f838a
LinaMarcela14 <lina.roldan@est.iudigital.edu.co>
2025-02-25 19:32:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup, find_packages
 setup(
     name="pad",
     version="0.0.1",
-    author="Andres Callejas",
-    author_email="",
+    author="Lina Marcela Roldan Pulgarin",
+    author_email="lina.roldan@est.iudigital.edu.co",
     description="",
     py_modules=["actividad_1"],
     install_requires=[
